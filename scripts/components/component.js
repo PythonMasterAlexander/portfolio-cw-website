1d2fc36eaf95ddb2a80fc07a594d71d5e33c016e
alexanderhardeland <alexander@hardeland.com>
2022-03-19 08:58:01 +0100

    Added font links - IBM MONO and IBM SANS. Added some default CSS styles

--- scripts/components/component.js
+++ scripts/components/component.js
@@ -1,3 +1,8 @@
+//By Alexander Hardeland
+//
+//
+//
+//
 //A list over my work places, I can easily change it up here
 export let workPlaces = [
   'Tunnel Arbeider',
@@ -5,3 +10,6 @@ export let workPlaces = [
   'Forskalings Snekker',
   'Lastebil Sjøfør'
 ];
+
+//A list over my education
+export let education = [];
